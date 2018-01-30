81562a128be697896ee20a36ab5ddf45e53978e3
Edd <site@edd.fm>
2018-01-30 18:43:33 +0000

    Add vscode extensions

---
+++ vscode/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+code --install-extension HookyQR.beautify
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dracula-theme.theme-dracula
+code --install-extension eamodio.gitlens
+code --install-extension flowtype.flow-for-vscode
+code --install-extension saviorisdead.theme-gruvbox-alt
+code --install-extension thongdong7.vscode-autoimport
+code --install-extension tomphilbin.gruvbox-themes
+code --install-extension vscodevim.vim
