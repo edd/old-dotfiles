99d2ee9774d58056ce22d477e81bfe4cfb748bfd
Edd Hannay <site@edd.fm>
2018-05-08 12:07:56 +0100

    Update VSCode extensions

--- vscode/install.sh
+++ vscode/install.sh
@@ -8,3 +8,10 @@ code --install-extension saviorisdead.theme-gruvbox-alt
 code --install-extension thongdong7.vscode-autoimport
 code --install-extension tomphilbin.gruvbox-themes
 code --install-extension vscodevim.vim
+code --install-extension Orta.vscode-jest
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension eg2.tslint
+code --install-extension infeng.vscode-react-typescript
+code --install-extension kisstkondoros.typelens
+code --install-extension rbbit.typescript-hero
+
