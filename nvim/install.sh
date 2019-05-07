b6ac007c35e4a2f0113e9b4818c8e3449ce66fbe
Edd Hannay <site@edd.fm>
2019-05-07 10:57:33 +0100

    Add install file

---
+++ nvim/install.sh
@@ -0,0 +1,4 @@
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+npm -g install typescript neovim
+npm install -g git+https://github.com/Perlence/tstags.git
+pip3 install pynvim
