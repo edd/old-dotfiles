23c3e9136c25543b12a39f51d0b4fbd59bcecbe2
Edd Hannay <site@edd.fm>
2019-05-07 10:36:57 +0100

    Fish config update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,14 @@
 set fish_greeting ""
 alias vi=nvim
 
-gpg-agent daemon
 export GPG_TTY=(tty)
-
 
 set -x -U GOPATH $HOME/go
 set -x -U GOBIN $GOPATH/bin
+
+function nvm
+   bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
+end
+
+set -x NVM_DIR ~/.nvm
+nvm use default --silent
